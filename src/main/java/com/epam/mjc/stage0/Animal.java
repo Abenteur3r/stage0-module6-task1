aabb51acdad2d2124b10ba960b0332197c318950
Ayan <ajan.kohan@gmail.com>
2023-05-30 04:24:53 +0600

    M6 task1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,23 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        String hasFurVal = (this.hasFur) ? "a" : "no";
+        String  numPawsVal = (this.numberOfPaws>1) ? "paws" : "paw";
+        return "This animal is mostly "+this.color+". It has "+this.numberOfPaws+" "+numPawsVal+" and "+ hasFurVal +" fur.";
+    }
+    @Override
+    public String toString(){
+        return getDescription();
+    }
 }
